689c8b7bed7002dabd4017a086e7c30abe705a79
Gasol <zhaomeng@testbird.com>
2023-01-10 17:01:28 +0800

    optimize

---
+++ live/src/main/java/com/alive/union/boost/TogetherReceiver.java
@@ -0,0 +1,12 @@
+package com.alive.union.boost;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class TogetherReceiver extends BroadcastReceiver {
+    @Override // android.content.BroadcastReceiver
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
